6379d95e072b10d485c428fbaafa56e6e8b15d3f
wanyuasam <samuelwanyua@gmail.com>
2024-08-21 14:02:40 +0300

    Post request to create a new user

--- my-app/src/store/features/personSlice.ts
+++ my-app/src/store/features/personSlice.ts
@@ -1,4 +1,7 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import axios from "axios"
+
+const url = 'https://jsonplaceholder.typicode.com/users'
 
 export interface Person{
     id: number
@@ -13,6 +16,33 @@ const initialState: PersonState ={
     persons: [],
 }
 
+export const fetchPersons = createAsyncThunk('person/fetchPersons', async (_, thunkAPI) => {
+    try{
+        const response = await axios.get(url)
+        return response.data
+    }
+    catch(error){
+        return thunkAPI.rejectWithValue(error.response.data)
+    }
+})
+
+// create a new person entry
+export const savePerson = createAsyncThunk('person/savePerson', async (name:string, thunkAPI) => {
+    try{
+        const response = await axios.post(url, {name})
+        return response.data
+    }
+    catch(error){
+        if(axios.isAxiosError(error)){
+            return thunkAPI.rejectWithValue(error.response?.data || "An error occured")
+        }
+        else{
+            return thunkAPI.rejectWithValue("An unexpected error occured")
+        }
+
+    }
+})
+
 export const PersonSlice = createSlice({
     name: "person",
     initialState,
@@ -22,8 +52,14 @@ export const PersonSlice = createSlice({
                 id: state.persons.length,
                 name: action.payload.name
             })
-        }
+        },
     },
+    extraReducers: (builder) => {
+        builder.addCase(fetchPersons.fulfilled, (state, action) => {
+            state.persons = action.payload
+        })
+
+    }
 })
 
 export const {addPerson} = PersonSlice.actions
